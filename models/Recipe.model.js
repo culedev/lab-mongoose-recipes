7aca706860eb7c26350c36d6cfb35ec4b0193012
Ivan Culebra <ivan.culebra.munera@gmail.com>
2022-07-21 16:52:08 +0200

    Iteration 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,41 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  level: [{
+    type: String,
+    enum: ["easy peasy", "amateur chef", "ultrapro chef"]
+  }],
+  ingredients: [{
+    type: String
+  }],
+  cuisine: {
+    type: String,
+    required: true,
+  },
+  dishType: [{
+    type: String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"]
+  }],
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    minimum: 0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: "2022-07-22"
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
